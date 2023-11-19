e2ff29940b798d2e23c3ac3cc04dbe80e8826acb
Barbara Machado <147945840+BarbarizandoDev@users.noreply.github.com>
2023-11-19 20:13:40 +0000

    atualizado em 19-11-23

---
+++ src/Calculadora/CalculadoraNutricional.java
@@ -0,0 +1,18 @@
+
+package Calculadora;
+
+
+
+public class CalculadoraNutricional {
+
+   
+
+    public static void main(String[] args) {
+        
+        TelaInnicial tela = new TelaInnicial ();
+        tela.setVisible(true);
+    }
+}
+
+    
+
